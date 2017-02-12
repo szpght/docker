8bf87e7a6322ffd779d1d3c860f7ec7ef0229f38
Brett Vickers <brett@beevik.com>
2017-02-12 12:26:47 -0800

    Add terraria container

---
+++ terraria/run.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+#----------------------------------------------------------------------
+# Configuration
+#     Modify the following variables to adjust your terraria setup.
+
+# PORT
+#    The port you want the terraria server to listen on
+PORT=7777
+
+# TERRARIA_DIR
+#    The directory containing terraria *.wld files
+TERRARIA_DIR=/home/brett/terraria/world
+
+# WORLD_FILE
+#    The .wld file within the world directory to run on the server
+WORLD_FILE=home.wld
+
+#----------------------------------------------------------------------
+
+docker run \
+	-d \
+	-p ${PORT}:7777 \
+	-v ${TERRARIA_DIR}:/world \
+	--name="terraria" \
+	brett/terraria:latest \
+	-world /world/${WORLD_FILE}
