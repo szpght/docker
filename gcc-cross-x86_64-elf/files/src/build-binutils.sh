a8aca6d05d1ee62e5b6ffea258b29d6ba030c463
Brett Vickers <brett@beevik.com>
2018-02-11 13:53:02 -0800

    Update to gcc 7.3 and binutils 2.30

--- gcc-cross-x86_64-elf/files/src/build-binutils.sh
+++ gcc-cross-x86_64-elf/files/src/build-binutils.sh
@@ -7,9 +7,9 @@ export PATH="$PREFIX/bin:$PATH"
 cd $PREFIX/src
 mkdir build-binutils
 cd build-binutils
-../binutils-2.29/configure --target=$TARGET --prefix="$PREFIX" --with-sysroot --disable-nls --disable-werror
+../binutils-2.30/configure --target=$TARGET --prefix="$PREFIX" --with-sysroot --disable-nls --disable-werror
 make -j 4
 make install
 
 cd $PREFIX/src
-rm -rf build-binutils.sh build-binutils binutils-2.29
+rm -rf build-binutils.sh build-binutils binutils-2.30
