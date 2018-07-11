87509fe049d237b8f6943167c72e7b9033533ae1
Brett Vickers <brett@beevik.com>
2018-07-11 14:56:41 -0700

    Update dynamodb dockerfile

--- redis/run.sh
+++ redis/run.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+mkdir -p volumes/data
 data=$(realpath volumes/data)
 docker run \
 	-d \
 	-p 6379:6379 \
+	--init \
+	--restart always \
 	-v ${data}:/data \
 	--name redis \
 	brett/redis \
